acab02151d915b7b791cb55c08334ad5d32eb41f
Liza-Mathur <lisamathur0803@gmail.com>
2025-02-24 22:07:34 +0530

    Updated few logics

--- StoreAndDetectFaces_FaceRecog.py
+++ StoreAndDetectFaces_FaceRecog.py
@@ -3,15 +3,17 @@ import face_recognition
 import os
 import matplotlib.pyplot as plt
 
-
+folder = 'StoredFaces'
 
 def storeFaces(img):
     face_locs = face_recognition.face_locations(img, model='hog')
     plt.imshow(img, cmap='gray')
     plt.show()
+    i = 1
     for top, right, bottom, left in face_locs:
         # cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
         # left - x, to - y , right - x+w , bottom - y+h
-        roi = img[max(top-5 , 0):max(bottom+5 , 0) , max(0 , left-5):max(0, right+5)]
-        plt.imshow(roi)
-        plt.show()
+        face = img[max(top-5 , 0):max(bottom+5 , 0) , max(0 , left-5):max(0, right+5)]
+        cv2.imwrite(os.path.join(folder, f"{i}.jpg"), face)
+        i += 1
+        
