89ec055bc211a29258a14a718632f56589081745
Liza-Mathur <lisamathur0803@gmail.com>
2025-02-24 22:15:25 +0530

    Now it will return embeddings too

--- StoreAndDetectFaces_FaceRecog.py
+++ StoreAndDetectFaces_FaceRecog.py
@@ -9,11 +9,13 @@ def storeFaces(img):
     face_locs = face_recognition.face_locations(img, model='hog')
     plt.imshow(img, cmap='gray')
     plt.show()
-    i = 1
-    for top, right, bottom, left in face_locs:
-        # cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
-        # left - x, to - y , right - x+w , bottom - y+h
-        face = img[max(top-5 , 0):max(bottom+5 , 0) , max(0 , left-5):max(0, right+5)]
-        cv2.imwrite(os.path.join(folder, f"{i}.jpg"), face)
-        i += 1
+    embeddings = face_recognition.face_encodings(img, face_locs)
+    return embeddings
+    # for top, right, bottom, left in face_locs:
+    #     # cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
+    #     # left - x, to - y , right - x+w , bottom - y+h
+    #     face = img[max(top-5 , 0):max(bottom+5 , 0) , max(0 , left-5):max(0, right+5)]
+    #     embeddings = face_recognition.face_e
+    #     cv2.imwrite(os.path.join(folder, f"{i}.jpg"), face)
+    #     i += 1
         
