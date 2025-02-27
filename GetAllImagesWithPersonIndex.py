1ea317d0ad6c24ff21516d88c5f6c0298df450b3
Liza-Mathur <lisamathur0803@gmail.com>
2025-02-27 19:43:40 +0530

    Get all images from person index in excel

--- GetAllImagesWithPersonIndex.py
+++ GetAllImagesWithPersonIndex.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-excel_file = "image_indexes.xlsx"
+
 
 def getImagesWithPersonIndex(personIdx , imageName):
     # indexes_str = ",".join(map(str, indexList))
@@ -23,25 +23,31 @@ def getImagesWithPersonIndex(personIdx , imageName):
 
     # print(f"Saved: {images} -> {indexList}")
 
-    
+    excel_file = "image_indexes.xlsx"
     try:
         df = pd.read_excel(excel_file , engine="openpyxl")
+        print("In try")
     except FileNotFoundError:
         print('In Except')
         return None
     
     images = df['Image'].tolist()
     indexes = df['Indexes'].apply(getList)
+    # if images:
+        # print(images)
     # print('images - ', images)
     # print('indexes - ', indexes)
     imagesWithPersonIdx = []
-    
+    personIdx = int(personIdx)
+    # print('Person idx type - ', type(personIdx))
     for i, image in enumerate(images):
         if image == imageName:
+            print("img == image_name, so continue")
             continue
         # print(indexes[i])
         # print(type(indexes[i]))
         for k in indexes[i]:
+            # print(f"in {k} - k getting index - {indexes[i]}")
             if k == personIdx:
                 print(f"Adding image - {image} becuase it has indexes - {k}")
                 imagesWithPersonIdx.append(image)
@@ -54,14 +60,14 @@ def getList(string):
         lists.append(int(k))
     return lists
 
-folder = 'Data/Images'
-img_name = '20201026_131428.jpg'
-personIdx = 2
-x = getImagesWithPersonIndex(personIdx, img_name)
-if x != None:
-    for k in x:
-        print("In K")
-        filepath = os.path.join(folder, k)
-        img = cv2.cvtColor(cv2.imread(filepath) , cv2.COLOR_BGR2GRAY)
-        plt.imshow(img, cmap='gray')
-        plt.show()
+# folder = 'Data/Images'
+# img_name = '20201026_131428.jpg'
+# personIdx = 2
+# x = getImagesWithPersonIndex(personIdx, img_name)
+# if x != None:
+#     for k in x:
+#         print("In K")
+#         filepath = os.path.join(folder, k)
+#         img = cv2.cvtColor(cv2.imread(filepath) , cv2.COLOR_BGR2GRAY)
+#         plt.imshow(img, cmap='gray')
+#         plt.show()
